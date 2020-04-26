b5dec3e4313ef73f2a380f427171d1a8a154058c
NHL6261 <248376261@qq.com>
2020-04-26 18:17:53 +0800

    first

---
+++ src/components/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <h1>这是登录页面</h1>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+  }
+</script>
+
+<style lang="stylus" scoped>
+
+ 
+</style>
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  lintOnSave: false, //关闭eslinet检查
+  devServer:{
+      open:true, // 自动打开浏览器
+      port:8888, // 修改端口号
+  }
+}
